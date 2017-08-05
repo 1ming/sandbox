8e410cd9983fe203b387d6d70a370dd3c3bccea9
Wanming Zhang <wanmingzhang@Wanmings-MacBook-Pro.local>
2017-08-04 23:49:05 -0700

    added messages

--- test1.py
+++ test1.py
@@ -3,3 +3,12 @@ print "Blue fish"
 n = raw_input("enter your colour: ")
 
 print n
+
+if n == "2":
+    print "You entered 2"
+elif n == "3":
+    print "You entered 3"
+else:
+    print "lobsters!!\n"
+
+print "\nend of message\n"
